a32c2e0898336c4b7e76bee3b9bf2125ac474e99
Hristo Kazanliev <118056676+HristoKazanliev@users.noreply.github.com>
2026-01-12 22:03:29 +0200

    feat: Refactor array operations and add validation for sorted items

--- C#-Part4/Arrays-with-helper-methods/TestProject/Program.cs
+++ C#-Part4/Arrays-with-helper-methods/TestProject/Program.cs
@@ -29,33 +29,77 @@
 //     Console.WriteLine($"-- {pallet}");
 // }
 
-string[] pallets =  ["B14", "A11", "B12", "A13" ];
-Console.WriteLine("");
+// string[] pallets =  ["B14", "A11", "B12", "A13" ];
+// Console.WriteLine("");
 
-Array.Clear(pallets, 0, 2);
-Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
-foreach (var pallet in pallets)
+// Array.Clear(pallets, 0, 2);
+// Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"-- {pallet}");
+// }
+
+// Console.WriteLine("");
+// Array.Resize(ref pallets, 6);
+// Console.WriteLine($"Resizing 6 ... count: {pallets.Length}");
+
+// pallets[4] = "C01";
+// pallets[5] = "C02";
+
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"-- {pallet}");
+// }
+
+// Console.WriteLine("");
+// Array.Resize(ref pallets, 3);
+// Console.WriteLine($"Resizing 3 ... count: {pallets.Length}");
+
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"-- {pallet}");
+// }
+
+// string value = "abc123";
+// char[] chars = value.ToCharArray();
+// Array.Reverse(chars);
+// // string result = new string(chars);
+// string result = String.Join(", ", chars);
+// Console.WriteLine(result);
+
+// string[] items = result.Split(',');
+// foreach (string item in items)
+// {
+//     Console.WriteLine(item);
+// }
+
+// string pangram = "The quick brown fox jumps over the lazy dog";
+
+// string[] arr = pangram.Split();
+// string[] arrToPrint = new string[pangram.Length];
+
+// for (int i = 0; i < arr.Length; i++)
+// {
+//     char[] chars = arr[i].ToCharArray();
+//     Array.Reverse(chars);
+//     arrToPrint[i] = new string(chars);
+// }     
+
+// string result = String.Join(" ", arrToPrint);
+// Console.WriteLine(result);
+
+string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+string[] sorted = orderStream.Split(",");
+Array.Sort(sorted);
+
+foreach (var item in sorted)
 {
-    Console.WriteLine($"-- {pallet}");
-}
-
-Console.WriteLine("");
-Array.Resize(ref pallets, 6);
-Console.WriteLine($"Resizing 6 ... count: {pallets.Length}");
-
-pallets[4] = "C01";
-pallets[5] = "C02";
-
-foreach (var pallet in pallets)
-{
-    Console.WriteLine($"-- {pallet}");
-}
-
-Console.WriteLine("");
-Array.Resize(ref pallets, 3);
-Console.WriteLine($"Resizing 3 ... count: {pallets.Length}");
-
-foreach (var pallet in pallets)
-{
-    Console.WriteLine($"-- {pallet}");
+    if (item.Length == 4)
+    {
+        Console.WriteLine(item);
+    }
+    else
+    {
+        Console.WriteLine(item + "\t- Error");
+    }
 }
