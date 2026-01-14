4f03e618e18061d9b94ace93436ed3f6b8306f63
Hristo Kazanliev <118056676+HristoKazanliev@users.noreply.github.com>
2026-01-14 23:10:27 +0200

    feat: Added Format alphanumeric data examples and update .gitignore

--- TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9efb8a38d5066569c740d476570a274170205edc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a32c2e0898336c4b7e76bee3b9bf2125ac474e99")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
