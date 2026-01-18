a53c2647bafb5960c1078342010527ee37d029f1
Hristo Kazanliev <118056676+HristoKazanliev@users.noreply.github.com>
2026-01-18 20:01:39 +0200

    feat: Completed Challenge project - Work with variable data in C#

--- TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f03e618e18061d9b94ace93436ed3f6b8306f63")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d859eb42608b5e130199b0e9c944f318151500b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
