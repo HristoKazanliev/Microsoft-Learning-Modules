2a4c52883f3c0afab18a9aab49cfb618024a58b7
Hristo Kazanliev <118056676+HristoKazanliev@users.noreply.github.com>
2026-02-15 23:55:34 +0200

    feat: Added Exercise - Create and throw an exception

--- TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0bab9c992a8dbe4e013e0eaedca7ae88ef4a30d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e43e241d4343e029eb205e0ea71b0a0a97ec704")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
