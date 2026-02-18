9a9ac686f8f8dcd41e4ffb70e9f53401cdcd74a3
Hristo Kazanliev <118056676+HristoKazanliev@users.noreply.github.com>
2026-02-18 23:58:02 +0200

    build: Add initial project structure

--- TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a4c52883f3c0afab18a9aab49cfb618024a58b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7337f85f2729c37673c1ed9b2c3d3904baedad62")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
