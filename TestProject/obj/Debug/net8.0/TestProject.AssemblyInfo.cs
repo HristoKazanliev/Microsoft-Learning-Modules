8826c8b17a91cca0dabcc9809d74840dbdd7308e
Hristo Kazanliev <118056676+HristoKazanliev@users.noreply.github.com>
2026-02-20 21:50:52 +0200

    feat: Added Challenge project Module

--- TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7337f85f2729c37673c1ed9b2c3d3904baedad62")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+020e18d83f61fe1e916009309d4e49b4a7599c75")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
