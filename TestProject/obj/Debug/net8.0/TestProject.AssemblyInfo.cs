63349feb58595d5af2029cf54eaf005e81c8d8d9
Hristo Kazanliev <118056676+HristoKazanliev@users.noreply.github.com>
2026-03-07 00:01:11 +0200

    build: Create a web UI with ASP.NET Core

--- TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8826c8b17a91cca0dabcc9809d74840dbdd7308e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f3c2ad1bbbc2845b32791aa0f054661e5427779")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
