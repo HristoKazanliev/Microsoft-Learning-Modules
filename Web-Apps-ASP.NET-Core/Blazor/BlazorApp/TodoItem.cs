40b347db74a9a131ee11d29e989665f71dbe3ffd
Hristo Kazanliev <118056676+HristoKazanliev@users.noreply.github.com>
2026-03-24 23:56:04 +0200

    feat: Add Todo functionality with item management and navigation link

---
+++ Web-Apps-ASP.NET-Core/Blazor/BlazorApp/TodoItem.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorApp
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public bool IsDone { get; set; } = false;
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
