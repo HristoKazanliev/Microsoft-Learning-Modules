e400825e89678a71d1347350ce0b207d0c8288af
Hristo Kazanliev <118056676+HristoKazanliev@users.noreply.github.com>
2026-03-25 00:18:13 +0200

    style: Bootstrap styling for Todo component layout and enhance Todo management features

--- Web-Apps-ASP.NET-Core/Blazor/BlazorApp/TodoItem.cs
+++ Web-Apps-ASP.NET-Core/Blazor/BlazorApp/TodoItem.cs
@@ -5,7 +5,6 @@
         public int Id { get; set; }
         public string? Title { get; set; }
         public bool IsDone { get; set; } = false;
-
         public DateTime CreatedAt { get; set; }
     }
 }
